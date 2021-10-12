b543157abef3a3cd83f6c71d0b1d1d07f5bdaffa
DevTreason <59517946+DevTreason@users.noreply.github.com>
2021-10-12 17:15:24 -0500

    Update Game.cs

--- code/Game.cs
+++ code/Game.cs
@@ -21,7 +21,7 @@ namespace SWBase
 	/// Your game needs to be registered (using [Library] here) with the same name 
 	/// as your game addon. If it isn't then we won't be able to find it.
 	/// </summary>
-	[Library( "SWBase" )]
+	[Library( "starwars" )]
 	public partial class SWGameBase : Sandbox.Game
 	{
 		public SWGameBase()
